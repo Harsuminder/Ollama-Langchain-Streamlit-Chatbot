f740ae0de95b3bd7cc1fb3e53cd8088c1861cea2
Harsuminder Kaur Gill <harsuminder@gmail.com>
2025-10-19 14:58:37 -0400

    Added chatbot.py and requirements.txt

---
+++ chatbot.py
@@ -0,0 +1,25 @@
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo with Gemma Model")
+input_text= st.text_input("What question you have in mind?")
+
+## Ollama llama 2 model
+llm= Ollama(model="gemma:2b")
+output_parser= StrOutputParser()
+chain= prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
